ad8d18cc0542c2e8c34a54773f619989d603029d
shuhaiwen <shuhaiwen94@gmail.com>
2021-03-09 20:22:04 +0800

    部署pwa.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,16 @@ module.exports = {
   dest: 'blogTemp',
   description: '小锋哥的博客',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/github.svg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    //['link', { rel: 'icon', href: '/github.svg' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/pwa_icon.jpg' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   markdown: {
     lineNumbers: true
